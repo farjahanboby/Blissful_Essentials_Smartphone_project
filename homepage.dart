7d83bc9c1508799987a0d621bcce6d7bf8f494f5
Nahian-Raisa <101518534+Nahian-Raisa@users.noreply.github.com>
2023-08-15 16:33:30 +0600

    homepage.dart
    Signed-off-by: Nahian-Raisa <101518534+Nahian-Raisa@users.noreply.github.com>

--- homepage.dart
+++ homepage.dart
@@ -1,140 +1,167 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-  void main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
-  class MyApp extends StatelessWidget {
-    const MyApp({super.key});
-    // This widget is the root of your application.
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.purple),
-        home: HomePage(title: 'xyz'),
-        debugShowCheckedModeBanner: false,
-      );
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: HomePage(title: 'xyz'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+
+    // Perform actions based on the selected tab
+    if (_currentIndex == 0) {
+      // Handle the Home tab
+      print('Home tab pressed');
+    } else if (_currentIndex == 1) {
+      // Handle the Cart tab
+      print('Cart tab pressed');
+    } else if (_currentIndex == 2) {
+      // Handle the Others tab
+      print('Others tab pressed');
     }
   }
 
-  class HomePage extends StatelessWidget {
-    final String title;
-    const HomePage({super.key, required this.title});
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFA964DF),
+      appBar: AppBar(
         backgroundColor: Color(0xFFA964DF),
-        appBar: AppBar(
-          backgroundColor: Color(0xFFA964DF),
-          title: Text(
-            "BLISSFUL ESSENTIALS",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 13,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ), // Set the background color to purple
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 60),
-              Container(
-                width: 300, // Adjust the width as needed
-                height: 300, // Adjust the height as needed
-                child: GridView.builder(
-                  itemCount: categories.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 10,
-                  ),
-                  itemBuilder: (context, index) {
-                    return CategoryCard(
-                      category: categories[index],
-                      image: categoryImages[index],
-                    );
-                    return Container(
-                      color: Colors.white,
-                    );
-                  },
-                ),
-              ),
-              SizedBox(height: 20),
-            ],
+        title: Text(
+          "BLISSFUL ESSENTIALS",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 13,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-            color: Colors.purple[200],
-            border: Border(
-              top: BorderSide(width: 2.0, color: Colors.black),
-              bottom: BorderSide(width: 2.0, color: Colors.black),
-              // Add a top border
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.chat,
+              color: Colors.white,
             ),
-          ),
-          child: BottomNavigationBar(
-            currentIndex: 0,
-            items: [
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home, color: Colors.black), label: "Home"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.message, color: Colors.black), label: "Chat"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.shopping_cart, color: Colors.black),
-                  label: "Cart"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.more_horiz, color: Colors.black),
-                  label: "Others")
-            ],
-            onTap: (int index) {
-              if (index == 0) {}
-              if (index == 1) {}
-              if (index == 2) {}
+            onPressed: () {
+              // Handle chat button press here
+              print('Chat button pressed');
+              // Add your chat functionality here
             },
           ),
-        ),
-      );
-    }
-  }
-
-  class CategoryCard extends StatelessWidget {
-    final String category;
-    final String image;
-
-    CategoryCard({required this.category, required this.image});
-
-    @override
-    Widget build(BuildContext context) {
-      return Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(0.0), // Change the circular shape
-        ),
+        ],
+      ),
+      body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            SizedBox(height: 60),
             Container(
-              width: double.infinity,
-              height: 100,
-              child: Image.asset(
-                image,
-                fit: BoxFit.cover, // Adjust the fit mode as needed
+              width: 300,
+              height: 300,
+              child: GridView.builder(
+                itemCount: categories.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                itemBuilder: (context, index) {
+                  return CategoryCard(
+                    category: categories[index],
+                    image: categoryImages[index],
+                  );
+                },
               ),
             ),
-            SizedBox(height: 15),
-            Text(category),
+            SizedBox(height: 20),
           ],
         ),
-      );
-    }
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped, // Set the callback function
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.more_horiz),
+            label: 'Others',
+          ),
+        ],
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.purple,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
+      ),
+    );
   }
+}
 
-  List<String> categories = [
-    'Makeup',
-    'Skincare',
-  ];
+class CategoryCard extends StatelessWidget {
+  final String category;
+  final String image;
+
+  CategoryCard({required this.category, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(0.0),
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 100,
+            child: Image.asset(
+              image,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(height: 15),
+          Text(category),
+        ],
+      ),
+    );
+  }
+}
+
+List<String> categories = [
+  'Makeup',
+  'Skincare',
   // Add more categories here
+];
 
-  List<String> categoryImages = [
-    'images/makeup.png',
-    'images/skincare.jpeg',
-  ];
+List<String> categoryImages = [
+  'images/makeup.png',
+  'images/skincare.jpeg',
+  // Add more image paths here
+];
